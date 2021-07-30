7f3ab208b538e9b039e79e9260ef5a161fbd2a20
LamenLuan <luan.leme.s35@gmail.com>
2021-07-30 14:11:45 -0300

    Criando a entidade usuario

---
+++ src/entidades/Usuario.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+/**
+ *
+ * @author luanl
+ */
+public class Usuario {
+    private String nome, cpf, dataNascimento, telefone, email, senha;
+    private boolean masculino, comorbidade;
+
+    public Usuario(
+        String nome, String cpf, String dataNascimento, String telefone,
+        String email, String senha, boolean masculino, boolean comorbidade
+    ) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+        this.telefone = telefone;
+        this.email = email;
+        this.senha = senha;
+        this.masculino = masculino;
+        this.comorbidade = comorbidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public boolean isMasculino() {
+        return masculino;
+    }
+
+    public void setMasculino(boolean masculino) {
+        this.masculino = masculino;
+    }
+
+    public boolean isComorbidade() {
+        return comorbidade;
+    }
+
+    public void setComorbidade(boolean comorbidade) {
+        this.comorbidade = comorbidade;
+    }
+    
+}
